158d8e38d2474d8ab85aa90f4212dac2c43ee0b8
Xurliman <jumamuratovahurliman8@gmail.com>
2024-03-18 00:06:25 +0500

    generated proto files for bank & bank_branch

--- pkg/v1/handler/grpc/bank_service.go
+++ pkg/v1/handler/grpc/bank_service.go
@@ -1,9 +1,5 @@
 package grpc
 
-import (
-	"github.com/Xurliman/banking-microservice/pkg/v1/interfaces"
-)
-
 type BankService struct {
-	bankCase interfaces.BankCaseInterface
+	bankCase interfaces.
 }
